71d1c15b293d21c5dbe04a6569902127123a6116
한상선 <hs94122@naver.com>
2022-02-16 20:06:24 +0900

    2022-02-16 20:06

--- src/main/java/com/javaex/vo/CategoryVo.java
+++ src/main/java/com/javaex/vo/CategoryVo.java
@@ -2,18 +2,19 @@ package com.javaex.vo;
 
 public class CategoryVo {
 
-	//필드
+	// 필드
 	private int cateNo;
 	private String id;
 	private String cateName;
 	private String description;
 	private String regDate;
 	private int count;
-	
-	//생성자
+
+	// 생성자
 	public CategoryVo() {
-		
+
 	}
+
 	public CategoryVo(int cateNo, String id, String cateName, String description, String regDate, int count) {
 		this.cateNo = cateNo;
 		this.id = id;
@@ -23,46 +24,56 @@ public class CategoryVo {
 		this.count = count;
 	}
 
-
-
-	//메소드g/s
+	// 메소드g/s
 	public int getCateNo() {
 		return cateNo;
 	}
+
 	public void setCateNo(int cateNo) {
 		this.cateNo = cateNo;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getCateName() {
 		return cateName;
 	}
+
 	public void setCateName(String cateName) {
 		this.cateName = cateName;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getRegDate() {
 		return regDate;
 	}
+
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
-	//메소드일반
+
+	// 메소드일반
 	@Override
 	public String toString() {
 		return "CategoryVo [cateNo=" + cateNo + ", id=" + id + ", cateName=" + cateName + ", description=" + description
