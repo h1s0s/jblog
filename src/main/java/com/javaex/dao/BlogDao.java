23a52a9bace607c7ecde26bf90554b8481a01bb4
한상선 <hs94122@naver.com>
2022-02-17 22:19:00 +0900

    2022-02-17 22:18

--- src/main/java/com/javaex/dao/BlogDao.java
+++ src/main/java/com/javaex/dao/BlogDao.java
@@ -11,22 +11,22 @@ public class BlogDao {
 
 	@Autowired
 	SqlSession sqlSession;
-	
-	//블로그 정보 가져오기
+
+	// 블로그 정보 가져오기
 	public BlogVo getBlog(String id) {
 		System.out.println("[BlogDao.getBlog]");
-		
+
 		BlogVo blogVo = sqlSession.selectOne("blog.getBlog", id);
-			
+
 		return blogVo;
 	}
-	
+
 	public void setBlog(BlogVo blogVo) {
 		System.out.println("[BlogDao.blogVo]");
-		int count = sqlSession.insert("blog.setBlog",blogVo);
-		System.out.println("["+count+"건이 생성되었습니다.(blog)]");
+		int count = sqlSession.insert("blog.setBlog", blogVo);
+		System.out.println("[" + count + "건이 생성되었습니다.(blog)]");
 	}
-	
+
 	public void update(BlogVo blogVo) {
 		System.out.println("[BlogDao.update]");
 		sqlSession.update("blog.update", blogVo);
